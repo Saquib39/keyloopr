aca10d9b029b1b9c4d5bd61f6b1239defedd3a2e
saquib39 <mohdsaquib619@gmail.com>
2026-02-01 16:29:25 +0530

    auth issue fixed

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -33,19 +33,10 @@ export async function POST(req) {
     { status: 200 },
   );
 
-  // response.cookies.set("token", token, {
-  //   httpOnly: true,
-  //   path: "/",
-  //   maxAge: 7 * 24 * 60 * 60, // 7 days
-  // });
-
   response.cookies.set("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
     path: "/",
-    maxAge: 7 * 24 * 60 * 60,
+    maxAge: 7 * 24 * 60 * 60, // 7 days
   });
-
   return response;
 }
